aa8aa6a493755485f7b40fd2d10ec6c00d67b636
MrGoots <134533117+MrGoots@users.noreply.github.com>
2023-10-01 22:38:46 -0700

    belly

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,7 +1,6 @@
 // Adapt the Gauge Chart from https://plot.ly/javascript/gauge-charts/ Links to an external site. to plot the weekly washing frequency of the individual.
 // You will need to modify the example gauge code to account for values ranging from 0 through 9.
 
-
 var arrColorsG = ["#5899DA", "#E8743B", "#19A979", "#ED4A7B", "#945ECF", "#13A4B4", "#525DF4", "#BF399E", "#6C8893", "white"];
 
 // Update the chart whenever a new sample is selected.
@@ -19,11 +18,12 @@ function buildMetadata(sample) {
         });
     });
 }
+// Gauge Chart Function
 
 function buildGaugeChart(sample) {
     console.log('sample', sample);
 
-    d3.json('samples.jason').then (data => {
+    d3.json('samples.json').then (data => {
         var objs = data.metadata;
         //console.lof('objs', objs);
 
@@ -34,6 +34,8 @@ function buildGaugeChart(sample) {
         buildGaugeChart(matchedSampleObj[0]);
     });
 }
+
+// Plot Gauge Chart
 
 function gaugeChart(data) {
     console.log('gaugeChart', data);
@@ -99,3 +101,107 @@ function gaugeChart(data) {
 
     Plotly.newPlot('gauge', trace. layout, {responsive: true});
 }
+
+// Use the D3 library to read in samples.json from the URL https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json.
+
+function buildMetadata(sample) {
+    d3.json('samples.json').then((data) => {
+        var metadata = data.metadata;
+        var resultsarray = metadata.filter(sampleobject =>
+            sampleobject.id == sample);
+        var result = resultsarray[0]
+        var panel = d3.select('#sample-metadata');
+        panel.html("");
+        Object.entries(result).forEach(([key, value]) => {
+            panel.append('h6').text(`${key}: ${value}`);
+        });
+    });
+}
+
+// Create a bubble chart that displays each sample.
+
+function buildCharts(sample) {
+d3.json('samples.json').then((data) => {
+    var samples = data.samples;
+    var resultsarray = samples.filter(sampleobject =>
+        sampleobject.id == sample);
+    var result = resultsarray[0]
+
+    var ids = result.otu_ids;
+    var labels = result.otu_labels;
+    var values = result.sample_values;
+
+    var LayoutBubble = {
+        margin: { t: 0},
+        xaxis: { title: 'OYU ID' },
+        hovermode: 'closest',
+        };
+
+    var DataBubble = [
+        {
+            x: ids,
+            y: values,
+            text: labels,
+            mode: 'markers',
+            marker: {
+                color: ids,
+                size: values,
+                }
+        }
+    ];
+        
+    Plotly.newPlot('bubble', DataBubble, LayoutBubble);
+
+// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+// Use sample_values as the values for the bar chart.
+
+    var bar_data = [
+        {
+            y: ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+            x: values.slice(0, 10).reverse(),
+            text: labels.slice(0, 10).reverse(),
+            type: 'bar',
+            orientation: 'h'
+        }
+    ];
+
+    var barLayout = {
+        title: "Top 10 | Bacteria",
+        margin: { t: 30, l: 150 }
+    };
+
+    Plotly.newPlot('bar', bar_data, barLayout);
+});
+}
+
+// Display each key-value pair from the metadata JSON object somewhere on the page.
+function init() {
+var selector = d3.select('#selDataset');
+
+d3.json('samples.json').then((data) => {
+    var sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+        selector
+            .append('option')
+            .text(sample)
+            .property('value', sample);
+
+    });
+
+    const firstSample = sampleNames[0];
+    buildMetadata(firstSample);
+    buildCharts(firstSample);
+    buildGaugeChart(firstSample)
+
+    });
+    }
+
+    function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+    buildGaugeChart(newSample)
+
+    }
+
+    init();
+    
